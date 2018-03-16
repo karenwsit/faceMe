27d1b6b32051753e85e00cf3af5d0fb761500fc5
Karen Sit <karenwsit@gmail.com>
2018-03-16 13:04:34 -0700

    gallery upload peer dependency resolved

--- src/Upload.js
+++ src/Upload.js
@@ -1,11 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
+
+import FineUploaderTraditional from 'fine-uploader-wrappers'
+import Gallery from 'react-fine-uploader'
+
+const uploader = new FineUploaderTraditional({
+    options: {
+        chunking: {
+            enabled: true
+        },
+        deleteFile: {
+            enabled: true,
+            endpoint: '/uploads'
+        },
+        request: {
+            endpoint: '/uploads'
+        },
+        retry: {
+            enableAuto: true
+        }
+    }
+})
 
 class Upload extends Component {
   render() {
     return (
       <div>
         <h2>Upload?</h2>
-        <p>Place holder for image upload</p>
+        <Gallery uploader={ uploader } />
       </div>
     );
   }
