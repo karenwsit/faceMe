a053dbc5d6b641c38f4ad701a75e98834ee9cd6d
Karen Sit <karenwsit@gmail.com>
2018-04-06 15:56:28 -0700

    upload POST connected to backend express!

--- server.js
+++ server.js
@@ -7,13 +7,6 @@ import upload from './routes/upload'
 const PORT = process.env.PORT || 8080;
 let app = express()
 
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 app.use('/', index)
 app.use('/about', about)
 app.use('/upload', upload)
