c3170c722c26db1dbd54730b3c50b54f6732b3af
Karen Sit <karenwsit@gmail.com>
2018-04-10 19:21:52 -0700

    used correct s3 wrapper instead of native

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,39 +1,53 @@
 import React, { Component } from "react"
 
-import qq from 'fine-uploader'
+import FineUploaderS3 from 'fine-uploader-wrappers/s3'
 import Gallery from 'react-fine-uploader'
 import 'react-fine-uploader/gallery/gallery.css'
 
-const uploader = new qq.s3.FineUploader({
+const uploader = new FineUploaderS3({
     options: {
-        chunking: {
-            enabled: true
-        },
-        deleteFile: {
-            enabled: true,
-            endpoint: '/upload'
-        }
         request: {
-            endpoint: 'uploadedphotostomatch.s3.amazonaws.com',
+            endpoint: 'http://fineuploadertest.s3.amazonaws.com',
             accessKey: process.env.S3_PUBLIC_ACCESS_KEY
         },
-        retry: {
-            enableAuto: true
-        },
         signature: {
-          endpoint: '/s3/signature'
-        },
-        uploadSuccess: {
-          endpoint: '/s3/success'
-        },
-        validation: {
-          allowedExtensions: ['gif', 'jpeg', 'jpg', 'png'],
-          acceptFiles: 'image/gif, image/jpeg, impage/png',
-          sizeLimit: 5000000,
-          itemLimit: 1
+            endpoint: "/upload"
         }
     }
 })
+
+// const queuedFileStatus = uploader.qq.status.QUEUED
+
+// const uploader = new qq.s3.FineUploader({
+//     options: {
+//         chunking: {
+//             enabled: true
+//         },
+//         deleteFile: {
+//             enabled: true,
+//             endpoint: '/upload'
+//         },
+//         request: {
+//             endpoint: 'uploadedphotostomatch.s3.amazonaws.com',
+//             accessKey: process.env.S3_PUBLIC_ACCESS_KEY
+//         },
+//         retry: {
+//             enableAuto: true
+//         },
+//         signature: {
+//           endpoint: '/s3/signature'
+//         },
+//         uploadSuccess: {
+//           endpoint: '/s3/success'
+//         },
+//         validation: {
+//           allowedExtensions: ['gif', 'jpeg', 'jpg', 'png'],
+//           acceptFiles: 'image/gif, image/jpeg, impage/png',
+//           sizeLimit: 5000000,
+//           itemLimit: 1
+//         }
+//     }
+// })
 
 class Upload extends Component {
   render() {
