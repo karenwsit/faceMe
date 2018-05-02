7c3daf2e6904697e083c866b4d91fe331d159554
Karen Sit <karenwsit@gmail.com>
2018-05-02 15:44:14 -0700

    wip - discovered pagination of fbi api

--- routes/fbi.js
+++ routes/fbi.js
@@ -3,12 +3,35 @@ import request from 'request-promise'
 
 var router = express.Router()
 
+
+
+
 router.get('/', async (req, res, next) => {
   try {
-    const fbiImages = request({
-      uri: 'https://api.fbi.gov/wanted/v1/list'
+    const response = await request({
+      uri: 'https://api.fbi.gov/wanted/v1/list',
+      qs: {
+        page: 37
+      }
     })
-    res.json(fbiImages)
+    const fbiImages = JSON.parse(response)
+    console.log('fbiImages.page', fbiImages.page)
+    console.log('LEN:', fbiImages.items.length)
+    let subjects = {}
+    const getSubjects = (item) => {
+      if (item.subjects) {
+        item.subjects.forEach((subject) => {
+          if (!subjects[subject]) {
+            subjects[subject] = 1
+          } else {
+            subjects[subject] += 1
+          }
+        })
+      }
+    }
+    fbiImages.items.forEach(getSubjects)
+    console.log('SUBJECTS:', subjects)
+    res.send(fbiImages)
   } catch (e) {
     next(e)
   }
