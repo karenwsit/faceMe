9ccf5df78fdc7cf0c3152692c443ac470d26d836
Karen Sit <karenwsit@gmail.com>
2018-05-02 19:41:52 -0700

    restructuring for api loop calls

--- routes/fbi.js
+++ routes/fbi.js
@@ -3,36 +3,44 @@ import request from 'request-promise'
 
 var router = express.Router()
 
+let allItems = []
+let subjects = {}
+let page = 1
 
-
-
-router.get('/', async (req, res, next) => {
-  try {
-    const response = await request({
-      uri: 'https://api.fbi.gov/wanted/v1/list',
-      qs: {
-        page: 37
+const getSubjects = (item) => {
+  if (item.subjects) {
+    item.subjects.forEach((subject) => {
+      if (!subjects[subject]) {
+        subjects[subject] = 1
+      } else {
+        subjects[subject] += 1
       }
     })
-    const fbiImages = JSON.parse(response)
+  }
+}
+
+router.get('/', async (req, res, next) => {
+  return request({
+    uri: 'https://api.fbi.gov/wanted/v1/list',
+    qs: {
+      page
+    }
+  })
+  .then(res => {
+    const fbiImages = JSON.parse(res)
+    const total = fbiImages.total
+    const itemsLen = fbiImages.items.length
+    const numberOfCalls = Math.round(total/itemsLen)
+    while (page < numberOfCalls) {
+      page++
+    }
+    console.log('fbiImages.total', fbiImages.total)
     console.log('fbiImages.page', fbiImages.page)
     console.log('LEN:', fbiImages.items.length)
-    let subjects = {}
-    const getSubjects = (item) => {
-      if (item.subjects) {
-        item.subjects.forEach((subject) => {
-          if (!subjects[subject]) {
-            subjects[subject] = 1
-          } else {
-            subjects[subject] += 1
-          }
-        })
-      }
-    }
     fbiImages.items.forEach(getSubjects)
-    console.log('SUBJECTS:', subjects)
     res.send(fbiImages)
-  } catch (e) {
+    })
+  .catch (e) {
     next(e)
   }
 })
---
+++ src/utils/fbi.js
@@ -0,0 +1,5 @@
+const fetchAllItems = () => {
+  let allItems = []
+
+
+}
