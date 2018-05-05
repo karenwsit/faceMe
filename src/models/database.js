983c45e01df0bc67d228355e1bf08d112c6a9ad5
Karen Sit <karenwsit@gmail.com>
2018-05-05 15:42:56 -0700

    table creationadded

--- src/models/database.js
+++ src/models/database.js
@@ -1,8 +1,13 @@
-const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/karensit';
+var pg = require('pg')
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/karensit'
 
-const client = new pg.Client(connectionString);
-client.connect();
-const query = client.query(
-  'CREATE TABLE fbi_wanted(id SERIAL PRIMARY KEY, uuid VARCHAR(100) UNIQUE NOT NULL, subject VARCHAR[], url VARCHAR(100), images JSON)');
-query.on('end', () => { client.end(); });
+const client = new pg.Client(connectionString)
+
+const buildTable = async () => {
+  await client.connect()
+  await client.query(
+    'CREATE TABLE fbi_wanted(id SERIAL PRIMARY KEY, uuid VARCHAR(100) UNIQUE NOT NULL, subject VARCHAR[], url VARCHAR(100), images JSON)')
+  await client.end()
+}
+
+buildTable()
