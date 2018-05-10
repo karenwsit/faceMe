21d9f7433d7e0eacece34940e419e15ac271a9a3
Karen Sit <karenwsit@gmail.com>
2018-05-10 16:49:13 -0700

    wip

---
+++ src/utils/face++.js
@@ -0,0 +1,48 @@
+require('../../env.js')
+var request = require('request-promise')
+var db = require('../db')
+
+const faceKey = process.env.FACEME_API_KEY
+const faceSecret = process.env.FACEME_API_SECRET
+console.log('process.env:', process.env)
+console.log('FACE KEYYYY:', faceKey)
+
+const faceSetURL = 'https://api-us.faceplusplus.com/facepp/v3/faceset/create'
+const detectFaceURL = 'https://api-us.faceplusplus.com/facepp/v3/detect'
+
+const createFaceSet = async () => {
+  try {
+    // let faceSet = await request({
+    //   api_key:
+    //   api_secret:
+    //   display_name:
+    // })
+  } catch (e) {
+    console.log('create FaceSet error:', e)
+  }
+}
+
+const testImage = 'https://s3-us-west-1.amazonaws.com/uploadedphotostomatch/06ceb71a-d42f-47c7-9b89-2a8eca2b0cd6.JPG'
+
+const detectFaceSet = async () => {
+  let options = {
+    method: 'POST',
+    uri: detectFaceURL,
+    qs: {
+      api_key: faceKey,
+      api_secret: faceSecret,
+      image_url: testImage
+    },
+    json: true
+  }
+  try {
+    let response = await request(options)
+    console.log('responsee for detect:', respones)
+
+  } catch (e) {
+    console.log('detect FaceSet error:', e)
+  }
+}
+
+
+module.exports = detectFaceSet
--- start.js
+++ start.js
@@ -1,10 +1,15 @@
 // Transpile all code following this line with babel and use 'env' (aka ES6) preset.
+require('./env')
 require('babel-register')({
     presets: [ 'env' ]
 })
 require("babel-core/register")
 require("babel-polyfill")
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({ silent: true })
+  console.log('process env node env:', process.env)
+}
+
 
 
 // Import the rest of our application.
