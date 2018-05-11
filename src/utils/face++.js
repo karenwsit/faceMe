f84f5a149e541778b3706c356c0473b1745b49a4
Karen Sit <karenwsit@gmail.com>
2018-05-11 16:41:31 -0700

    query completed for original images

--- src/utils/face++.js
+++ src/utils/face++.js
@@ -5,17 +5,25 @@ var db = require('../db')
 
 const faceKey = process.env.FACEME_API_KEY
 const faceSecret = process.env.FACEME_API_SECRET
+const faceSetToken = process.env.FACE_SET_TOKEN
 
-const faceSetURL = 'https://api-us.faceplusplus.com/facepp/v3/faceset/create'
+const createFaceSetURL = 'https://api-us.faceplusplus.com/facepp/v3/faceset/create'
 const detectFaceURL = 'https://api-us.faceplusplus.com/facepp/v3/detect'
 
-const createFaceSet = async () => {
+const createFaceSetToken = async () => {
+  let options = {
+    method: 'POST',
+    uri: createFaceSetURL,
+    qs: {
+      api_key: faceKey,
+      api_secret: faceSecret
+    },
+    json: true
+  }
   try {
-    // let faceSet = await request({
-    //   api_key:
-    //   api_secret:
-    //   display_name:
-    // })
+    let response = await request(options)
+    const { faceset_token } = response
+    return faceset_token
   } catch (e) {
     console.log('create FaceSet error:', e)
   }
@@ -23,26 +31,49 @@ const createFaceSet = async () => {
 
 const testImage = 'https://s3-us-west-1.amazonaws.com/uploadedphotostomatch/06ceb71a-d42f-47c7-9b89-2a8eca2b0cd6.JPG'
 
-const detectFaceSet = async () => {
+const detectFaceSet = async (image_url) => {
   let options = {
     method: 'POST',
     uri: detectFaceURL,
     qs: {
       api_key: faceKey,
       api_secret: faceSecret,
-      image_url: testImage
+      image_url
     },
     json: true
   }
   try {
     let response = await request(options)
-    console.log('responsee for detect:', JSON.stringify(response))
+    const { faces, image_id } = response
+    console.log('faces:', faces)
+    console.log('image_id:', image_id)
 
   } catch (e) {
     console.log('detect FaceSet error:', e)
   }
 }
 
+const QUERY =
+`SELECT
+    url,
+    array_agg(each_attribute ->> 'original') images
+  FROM fbi_wanted
+  CROSS JOIN json_array_elements(images) each_attribute
+  WHERE (each_attribute -> 'original') is NOT NULL
+  GROUP BY url
+  LIMIT 6;
+`
+
+const queryImages = async () => {
+  await db.query(QUERY, (err, result) => {
+    if (err) {
+      console.log(err)
+    }
+    const { rows } = result
+    console.log('ROWS:', JSON.stringify(rows))
+  })
+}
+
 
 // module.exports = detectFaceSet
-detectFaceSet()
+queryImages()
